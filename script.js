e00dfc3601ebb591c52f4c3b4930649c526bc979
syedmin84 <syedmin84@gmail.com>
2025-05-17 18:15:39 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,26 @@
+
+const display = document.getElementById('display');
+const buttons = document.querySelectorAll('button');
+
+console.log(buttons)
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        if (button.textContent === 'AC') {
+            display.textContent = '0';
+        } else if (button.textContent === 'DEL') {
+            display.textContent = display.textContent.slice(0, -1) || '0';
+        } else if (button.textContent === '=') {
+            try {
+                display.textContent = eval(display.textContent);
+            } catch {
+                display.textContent = 'Error';
+            }
+        } else {
+            if (display.textContent === '0') {
+                display.textContent = button.textContent;
+            } else {
+                display.textContent += button.textContent;
+            }
+        }
+    });
+});
